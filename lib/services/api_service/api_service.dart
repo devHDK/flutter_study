96e367018a7ac0adb960b30325b5d8b9c8f8461a
devHDK <hyundo91082@gmail.com>
2023-03-13 19:47:26 +0900

    detail view model, api 호출

--- lib/services/api_service/api_service.dart
+++ lib/services/api_service/api_service.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 
 class ApiService {
   static const String baseURL =
@@ -21,6 +23,31 @@ class ApiService {
       }
 
       return webtoonInstancces;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseURL/$id');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatesEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstance = [];
+    final url = Uri.parse('$baseURL/$id/episodes');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstance.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstance;
     }
     throw Error();
   }
